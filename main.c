b45fa32abce626309a80eda428a88418ca606c3b
noahwekesa <noahwekesa0@gmail.com>
2023-05-25 12:49:35 +0300

    g

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
 #include "shell.h"
 
 /**
- * _main - program that entry point to the program
- * @argmc: The number of arguments passed to the program
- * @argmp: the array of the arguments passed to the program
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: if success return 0, if error return 1
+ * Return: 0 on success, 1 on error
  */
-int _main(int argmc, char **argmp)
+int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 
-	if (argmc == 2)
+	if (ac == 2)
 	{
-		fd = open(argmp[1], O_RDONLY);
+		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(argmp[0]);
+				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(argmp[1]);
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
@@ -39,6 +39,6 @@ int _main(int argmc, char **argmp)
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, argmp);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
